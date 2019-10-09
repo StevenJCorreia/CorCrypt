5063b203195874ff4a8904a85cd026be526415cb
StevenJCorreia <stevenjosephcorreia@gmail.com>
2019-10-09 16:30:59 -0400

    Added UI and functionality to main form

---
+++ MainPage.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace CorCrypt
+{
+    partial class CorCrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFile = new System.Windows.Forms.Button();
+            this.passwordValue = new System.Windows.Forms.TextBox();
+            this.filePathValue = new System.Windows.Forms.TextBox();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filePathErrorLabel = new System.Windows.Forms.Label();
+            this.passwordErrorLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectFile
+            // 
+            this.selectFile.Location = new System.Drawing.Point(321, 129);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(116, 36);
+            this.selectFile.TabIndex = 0;
+            this.selectFile.Text = "Select File";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // passwordValue
+            // 
+            this.passwordValue.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.passwordValue.Location = new System.Drawing.Point(197, 233);
+            this.passwordValue.MaxLength = 32;
+            this.passwordValue.Name = "passwordValue";
+            this.passwordValue.Size = new System.Drawing.Size(365, 35);
+            this.passwordValue.TabIndex = 1;
+            this.passwordValue.Text = "Enter Password";
+            this.passwordValue.TextChanged += new System.EventHandler(this.PasswordValue_TextChanged);
+            this.passwordValue.Enter += new System.EventHandler(this.PasswordValue_Enter);
+            this.passwordValue.Leave += new System.EventHandler(this.PasswordValue_Leave);
+            // 
+            // filePathValue
+            // 
+            this.filePathValue.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.filePathValue.Location = new System.Drawing.Point(197, 171);
+            this.filePathValue.Name = "filePathValue";
+            this.filePathValue.Size = new System.Drawing.Size(365, 35);
+            this.filePathValue.TabIndex = 2;
+            this.filePathValue.Text = "Enter File Path";
+            this.filePathValue.Enter += new System.EventHandler(this.FilePathValue_Enter);
+            this.filePathValue.Leave += new System.EventHandler(this.FilePathValue_Leave);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(373, 295);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(94, 36);
+            this.decryptButton.TabIndex = 3;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(273, 295);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(94, 36);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "Open File...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.aboutToolStripMenuItem.Text = "About CorCrypt...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // filePathErrorLabel
+            // 
+            this.filePathErrorLabel.AutoSize = true;
+            this.filePathErrorLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filePathErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.filePathErrorLabel.Location = new System.Drawing.Point(347, 209);
+            this.filePathErrorLabel.Name = "filePathErrorLabel";
+            this.filePathErrorLabel.Size = new System.Drawing.Size(45, 21);
+            this.filePathErrorLabel.TabIndex = 8;
+            this.filePathErrorLabel.Text = "error";
+            // 
+            // passwordErrorLabel
+            // 
+            this.passwordErrorLabel.AutoSize = true;
+            this.passwordErrorLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.passwordErrorLabel.Location = new System.Drawing.Point(347, 271);
+            this.passwordErrorLabel.Name = "passwordErrorLabel";
+            this.passwordErrorLabel.Size = new System.Drawing.Size(45, 21);
+            this.passwordErrorLabel.TabIndex = 9;
+            this.passwordErrorLabel.Text = "error";
+            // 
+            // CorCrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.passwordErrorLabel);
+            this.Controls.Add(this.filePathErrorLabel);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.filePathValue);
+            this.Controls.Add(this.passwordValue);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "CorCrypt";
+            this.Text = "MainPage";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.TextBox passwordValue;
+        private System.Windows.Forms.TextBox filePathValue;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.Label filePathErrorLabel;
+        private System.Windows.Forms.Label passwordErrorLabel;
+    }
+}
+
---
+++ MainPage.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CorCrypt
+{
+    public partial class CorCrypt : Form
+    {
+        private string FILE_PATH_VALUE_DEFAULT = "Enter File Path";
+        private string PASSWORD_VALUE_DEFAULT = "Enter Password";
+
+        public CorCrypt()
+        {
+            InitializeComponent();
+
+            filePathErrorLabel.Visible = false;
+            passwordErrorLabel.Visible = false;
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string projectRepo = "https://github.com/StevenJCorreia/CorCrypt";
+            Process.Start(projectRepo);
+        }
+
+        private void ResetForm()
+        {
+            filePathValue.Text = FILE_PATH_VALUE_DEFAULT;
+            GreyFilePathLabel();
+            passwordValue.Text = PASSWORD_VALUE_DEFAULT;
+            GreyPasswordLabel();
+
+            filePathErrorLabel.Visible = false;
+            passwordErrorLabel.Visible = false;
+        }
+
+        private void DarkenFilePathLabel()
+        {
+            filePathValue.Text = "";
+            filePathValue.ForeColor = Color.Black;
+        }
+
+        private void DarkenPasswordLabel()
+        {
+            passwordValue.Text = "";
+            passwordValue.PasswordChar = '*';
+            passwordValue.ForeColor = Color.Black;
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+            {
+                return;
+            }
+            else
+            {
+                // TODO - Decrypt
+
+                // TODO - Messagebox result
+
+                ResetForm();
+            }
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+            {
+                return;
+            }
+            else
+            {
+                // TODO - Encrypt
+
+                // TODO - Messagebox result
+
+                ResetForm();
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FilePathValue_Enter(object sender, EventArgs e)
+        {
+            if (filePathValue.Text == FILE_PATH_VALUE_DEFAULT)
+            {
+                DarkenFilePathLabel();
+            }
+        }
+
+        private void FilePathValue_Leave(object sender, EventArgs e)
+        {
+            if (filePathValue.Text == "")
+            {
+                GreyFilePathLabel();
+            }
+        }
+
+        private void GreyFilePathLabel()
+        {
+            filePathValue.Text = FILE_PATH_VALUE_DEFAULT;
+            filePathValue.ForeColor = Color.Silver;
+        }
+
+        private void GreyPasswordLabel()
+        {
+            passwordValue.Text = PASSWORD_VALUE_DEFAULT;
+            passwordValue.PasswordChar = '\0';
+            passwordValue.ForeColor = Color.Silver;
+        }
+
+        private void OpenFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectFile();
+        }
+
+        private void PasswordValue_Enter(object sender, EventArgs e)
+        {
+            if (passwordValue.Text == PASSWORD_VALUE_DEFAULT)
+            {
+                DarkenPasswordLabel();
+            }
+        }
+
+        private void PasswordValue_Leave(object sender, EventArgs e)
+        {
+            if (passwordValue.Text == "")
+            {
+                GreyPasswordLabel();
+            }
+        }
+
+        private string SelectFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Open File...";
+            openFileDialog.Filter = "All files (*.*)|*.*|CSV files (*.csv)|*.csv|Text files (*.txt)|*txt";
+            //saveFileDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.CheckFileExists = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return "";
+            }
+
+            return openFileDialog.FileName; // TODO - Make this return string[] for multiple file functionality
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            string filePath = SelectFile();
+
+            if (!(filePath.Length == 0))
+            {
+                filePathValue.Text = filePath;
+                filePathValue.ForeColor = Color.Black;
+            }
+        }
+
+        private Boolean ValidateForm()
+        {
+            Boolean formValid = true;
+
+            if (filePathValue.Text == FILE_PATH_VALUE_DEFAULT)
+            {
+                string error = "Please enter a file.";
+
+                SetErrorLabel(filePathErrorLabel, error);
+
+                formValid = false;
+            }
+
+            if (passwordValue.Text == PASSWORD_VALUE_DEFAULT)
+            {
+                string error = "Please enter a password.";
+
+                SetErrorLabel(passwordErrorLabel, error);
+
+                formValid = false;
+            }
+
+            return formValid;
+        }
+
+        private void SetErrorLabel(Label label, string error)
+        {
+            label.Text = error;
+            label.Visible = true;
+        }
+
+        private void PasswordValue_TextChanged(object sender, EventArgs e)
+        {
+            if (passwordValue.Text == PASSWORD_VALUE_DEFAULT || passwordValue.Text.Length == 0)
+            {
+                return;
+            }
+
+            passwordErrorLabel.Visible = false;
+
+            // Add more exception handling for weak passwords
+            if (passwordValue.Text.Length < 8)
+            {
+                string error = "Weak password detected.";
+
+                // TODO - Make look more like a warning than an error
+                SetErrorLabel(passwordErrorLabel, error);
+
+                return;
+            }
+        }
+    }
+}
